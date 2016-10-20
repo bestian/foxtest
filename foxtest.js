2dea67488bdce9c79c782f5a8bdf44ef0aebab32
Bestian Tang <besitan@gmail.com>
2016-10-20 13:48:27 +0800

    test

--- foxtest.js
+++ foxtest.js
@@ -1,4 +1,4 @@
-document.body.style.border = "10px solid gold"; // blue
+document.body.style.border = "5px solid gold"; // blue
 
 console.log("This is an informational message");
 
@@ -20,12 +20,37 @@ invis(zs,0);
 
 
 var goods = document.getElementsByClassName('rt-store-goods-disp-mix');
+var invalids = new Array;
+
+var hideList = ['osaka_shop',
+				'nagi83510',
+				'alanlin12345',
+				'nomatter01',
+				'cw162535',
+				'darkstars555',
+				'tsubaki2007',
+				'niangniang_m',
+				'book_210',
+				'simiyasan'];
+
+var re = new RegExp(hideList.join("|"));
+
 for (var i = 0; i < goods.length; i++) {
 	var g = goods[i];
-	if (g.innerHTML.match(/(osaka_shop|nagi83510|alanlin12345|nomatter01|cw162535|darkstars555|tsubaki2007|niangniang_m|book_210|simiyasan)/)) {
-		g.style.display = 'none';
+	if (g.innerHTML.match(re)) {
+		invalids.push(g);
+	}	
+}
+
+
+var visible = invalids.length > goods.length * 0.8 ? true : false;
+
+for (var i = 0; i < invalids.length; i++) {
+	var v = invalids[i];
+	v.style.opacity = '0.2';
+	if (!visible) {
+		v.style.display = 'none';
 		console.log('Hidden');
-		//g.style.opacity = '0.2';
 	}
 } 
 
@@ -38,7 +63,6 @@ for (var i = 0; i < imgs.length; i++) {
 		m.style.opacity = '0.1';
 	}
 } 
-
 
 
 console.log('ccc');
