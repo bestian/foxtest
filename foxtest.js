08234064579552e7e64a8f244a33a6e11c816ef5
Bestian Tang <besitan@gmail.com>
2016-10-20 12:34:47 +0800

    test

--- foxtest.js
+++ foxtest.js
@@ -1,1 +1,20 @@
-document.body.style.border = "5px solid red";
+document.body.style.border = "10px solid gold"; // blue
+
+console.log("This is an informational message");
+
+var invis = function(os) {
+	for (var i = 0; i < os.length; i++) {
+	    os[i].style.opacity = '0.1'; // 0.1
+	}
+}
+
+var xs = document.getElementsByClassName('bd');
+var ys = document.getElementsByClassName('adslot');
+
+invis(xs);
+invis(ys);
+
+
+
+
+console.log('ccc');
